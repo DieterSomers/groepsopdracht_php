d6c1112b28a15743e196486f58339c189cd14385
larskerstens <larskerstens10@gmail.com>
2021-01-22 23:39:56 +0100

    elke pagina standaard layout gegeven + links done

--- 0_Padel_Website/src/php/padel_bookings.php
+++ 0_Padel_Website/src/php/padel_bookings.php
@@ -1,1 +1,9 @@
 <?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once "../lib/autoload.php";
+PrintHeader();
+print "<main><h2>Dit is de bookings page</h2></main>";
+PrintFooter();
+?>
--- 0_Padel_Website/src/php/padel_home.php
+++ 0_Padel_Website/src/php/padel_home.php
@@ -1,2 +1,9 @@
 <?php
-echo 'dit is de home';
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once "../lib/autoload.php";
+PrintHeader();
+print "<main><h2>Dit is de home page</h2></main>";
+PrintFooter();
+?>
