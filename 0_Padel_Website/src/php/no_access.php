5a833e7ae69c864bca7f43e8a99efcbed0682eb1
larskerstens <larskerstens10@gmail.com>
2021-01-23 01:57:32 +0100

    final css

--- 0_Padel_Website/src/php/no_access.php
+++ 0_Padel_Website/src/php/no_access.php
@@ -5,6 +5,6 @@ ini_set( 'display_errors', 1 );
 $public_access = true;
 require_once "../lib/autoload.php";
 PrintHeaderNoAccess();
-        print "<main><h2>Oei u hebt naast de bal geslagen en hebt helaas geen toegang! U kan wel <a href=../../index.php>inloggen</a> Of <a href=./padel_register.php>registreren</a></h2></main>";
+        print "<main class='container'><h2>Oei u hebt naast de bal geslagen en hebt helaas geen toegang! U kan wel <a href=../../index.php>inloggen</a> Of <a href=./padel_register.php>registreren</a></h2></main>";
 PrintFooter();
         ?>
--- 0_Padel_Website/src/php/padel_profile.php
+++ 0_Padel_Website/src/php/padel_profile.php
@@ -4,6 +4,6 @@ ini_set( 'display_errors', 1 );
 
 require_once "../lib/autoload.php";
 PrintHeader();
-print "<main><h2>Dit is de profile page</h2></main>";
+print "<main class='container'><h2>Dit is de profile page</h2></main>";
 PrintFooter();
 ?>
